f3ffbf300c037af93e1267d0f8733c4ba2ce8966
Hyunmin Kim <125753636+hyunmn@users.noreply.github.com>
2024-10-12 09:02:40 -0400

    oct 12

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('dark mode');
+
+
+$(document).ready(function(){
+
+	// adds a a darkmode class to the body
+	//------------------------------------
+	$("#darkmode").click(function(){
+		$('body').toggleClass('darkmode');
+	});
+
+});
